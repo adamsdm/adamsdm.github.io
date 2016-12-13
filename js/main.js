964517c5dfb7bbdd17227fccaa5e6bf646baaed8
adam söderström <adam sderstrm>
2016-12-13 22:57:23 +0100

    Style updates

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     });
 
     $(document).on('scroll', function() {
-        if(offset>document.documentElement.scrollTop)
+        if (offset > document.documentElement.scrollTop)
             console.log("up");
         else
             console.log("down");
@@ -27,11 +27,11 @@ $(document).ready(function() {
         updateElements();
     })
 
-    function updateElements(){
-        parallax( $("#welcText"), offset, -0.4);
-        parallax( $("#welc-image"), offset, -0.4);
-        oppacity( $('.center'), 0, 400, offset);
-        blur( $("#sec-1 #bg"), offset);
+    function updateElements() {
+        parallax($("#welcText"), offset, -0.4);
+        parallax($("#welc-image"), offset, -0.4);
+        oppacity($('.center'), 0, 400, offset);
+        blur($("#sec-1 #bg"), offset);
     }
 
 
@@ -41,24 +41,32 @@ $(document).ready(function() {
 
     function blur(element, offset) {
         var pixs = Math.min(offset / 50, 4.0);
-        element.css({ 
-            "-webkit-filter"    : "blur(" + pixs + "px)", "filter": "blur(" + pixs + "px)",
-            "filter"            : "blur(" + pixs + "px)", "filter": "blur(" + pixs + "px)" 
- 
+        element.css({
+            "-webkit-filter": "blur(" + pixs + "px)",
+            "filter": "blur(" + pixs + "px)",
+            "filter": "blur(" + pixs + "px)",
+            "filter": "blur(" + pixs + "px)"
+
 
         })
     }
 
     function oppacity(element, fadeStart, fadeUntil, off) {
-        if (offset <= fadeStart) { opacity = 1; } else if (offset <= fadeUntil) { opacity = 1 - offset / fadeUntil; } else { opacity = 0; }
+        if (offset <= fadeStart) {
+            opacity = 1;
+        } else if (offset <= fadeUntil) {
+            opacity = 1 - offset / fadeUntil;
+        } else {
+            opacity = 0;
+        }
         element.css('opacity', opacity);
     }
 
     function parallax(element, off, speed) {
-        element.css({ 
-            "-webkit-transform" : 'translate(0px, ' + String(speed * off) + 'px)', 
-            "-ms-transform"     : 'translate(0px, ' + String(speed * off) + 'px)',
-            "transform"         : 'translate(0px, ' + String(speed * off) + 'px)' 
+        element.css({
+            "-webkit-transform": 'translate(0px, ' + String(speed * off) + 'px)',
+            "-ms-transform": 'translate(0px, ' + String(speed * off) + 'px)',
+            "transform": 'translate(0px, ' + String(speed * off) + 'px)'
 
         })
     }
